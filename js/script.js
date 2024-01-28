45a0447c91c77be62a6ca3e187c7da7d8289301c
Pavel Paulavets <paulavetspavel@outlook.com>
2024-01-28 15:40:29 +0300

    Refactoring code for geting points

--- js/script.js
+++ js/script.js
@@ -1,71 +1,87 @@
 import {
-  clearCanvas, clearAnswerPlace,
-  openPage, closePage, createCoordsSystemOnCanvas,
-  createCanvas, createContext, showAnswerForPointAndLine,
-  showAnswerForLineAndLine,showAnswerForPointAndSimpleFigure
+    clearCanvas, clearAnswerPlace,
+    openPage, closePage, createCoordsSystemOnCanvas,
+    createCanvas, createContext, showAnswerForPointAndLine,
+    showAnswerForLineAndLine, showAnswerForPointAndSimpleFigure
 } from './function.js';
 
 
 const X_MIN = -2;
 const X_MAX = 50;
+// для удаления скобок и запятых в textarea.value для создания массива точек
+const regExpDelBrackets = /[();, ]/;
+
 // ---- FOR MAIN ----
 const pages = document.querySelector('.pages');
 const closeBtn = document.querySelector('.close-btn');
-let currentWin;
-let inputs;
-let canvasPlace;
-let answerPlace;
+let currentWin = null;
+let form = null;
+let inputs = null;
+let inputsValue = null;
+let textareaValue = null;
+let canvasPlace = null;
+let answerPlace = null
+;
 // ---- FOR MAIN ----
 // открытие окна
 pages.addEventListener('click', (event) => {
-  event.preventDefault();
-  currentWin = openPage(event, closeBtn);
-  // создание канвы
-  canvasPlace = currentWin.querySelector('.picture');
-  const canvas = createCanvas(canvasPlace, 525, 525);
-  const ctx = createContext(canvas);
-  // создание координатных осей
-  createCoordsSystemOnCanvas(canvas, ctx);
-  // место для ответа
-  answerPlace = currentWin.querySelector('.answer');
-  inputs = currentWin.querySelector('form').querySelectorAll('input[type=number]');
-  const btnSubmit = currentWin.querySelector('.btn');
-  btnSubmit.addEventListener('click', (event) => {
     event.preventDefault();
-    event.stopPropagation();
-    clearCanvas(canvas, ctx);
-    clearAnswerPlace(answerPlace);
-    
-    const points = [];
-    inputs.forEach((input, index) => {
-      if(index % 2 === 0) {
-        points.push([ +inputs[ index ].value, +inputs[ index + 1 ].value ]);
-      }
+    currentWin = openPage(event, closeBtn);
+    form = currentWin.querySelector('form');
+    // создание канвы
+    canvasPlace = currentWin.querySelector('.picture');
+    const canvas = createCanvas(canvasPlace, 525, 525);
+    const ctx = createContext(canvas);
+    // создание координатных осей
+    createCoordsSystemOnCanvas(canvas, ctx);
+    // место для ответа
+    answerPlace = currentWin.querySelector('.answer');
+
+    inputs = form.querySelectorAll('input[type=number]');
+
+
+    const btnSubmit = currentWin.querySelector('.btn');
+    btnSubmit.addEventListener('click', (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        clearCanvas(canvas, ctx);
+        clearAnswerPlace(answerPlace);
+
+        // т. к. querySelectorAll всегда возвращает NodeList
+        inputsValue = (inputs.length !== 0) ? Array.from(inputs).map(input => input.value) : null;
+        // querySelector вернет undefined если не найдет textarea
+        textareaValue = form.querySelector(`textarea[name='pointsCoords']`)?.value
+            .split(regExpDelBrackets).filter(elem => elem !== '');
+
+        const points = [];
+        (inputsValue ?? textareaValue).forEach((element, index, array) => {
+            if (index % 2 === 0) {
+                points.push([+element, +(array[index + 1])]);
+            }
+        });
+
+        if (btnSubmit.classList.contains('pointAndLine')) {
+            showAnswerForPointAndLine(points, answerPlace, canvas, ctx, X_MIN, X_MAX);
+        }
+        if (btnSubmit.classList.contains('lineAndLine')) {
+            showAnswerForLineAndLine(points, answerPlace, canvas, ctx);
+        }
+        if (btnSubmit.classList.contains('pointAndSimpleFigure')) {
+            showAnswerForPointAndSimpleFigure(points, answerPlace, canvas, ctx);
+        }
     });
-    
-    if(btnSubmit.classList.contains('pointAndLine')) {
-      showAnswerForPointAndLine(points, answerPlace, canvas, ctx, X_MIN, X_MAX);
-    }
-    if(btnSubmit.classList.contains('lineAndLine')) {
-      showAnswerForLineAndLine(points, answerPlace, canvas, ctx);
-    }
-    if(btnSubmit.classList.contains('pointAndSimpleFigure')) {
-      const points=[[2,6],[5,7],[8,9],[4,8],[7,2],[5,4],[5,5],[4,10]]
-      showAnswerForPointAndSimpleFigure(points, answerPlace, canvas, ctx);
-    }
-  });
 });
 // закрытие окна
 closeBtn.addEventListener('click', (event) => {
-  event.preventDefault();
-  event.stopPropagation();
-  closePage(closeBtn);
-  canvasPlace.innerHTML = '';
-  answerPlace.innerHTML = '';
-  
-  for(const input of inputs) {
-    input.value = '';
-  }
+    event.preventDefault();
+    event.stopPropagation();
+    closePage(closeBtn);
+    canvasPlace.innerHTML = '';
+    answerPlace.innerHTML = '';
+
+    for (const input of inputs) {
+        input.value = '';
+    }
 });
 
 
